82918901f3492f027a0e25dbdbb6a679c53b783e
Omkar Gore <yothisisokg@gmail.com>
2026-04-08 22:57:18 +0530

    Update func1.py

--- func1.py
+++ func1.py
@@ -53,3 +53,47 @@ def outer():
 
 outer()
 print("Global:", x)
+
+
+
+#Using Lambda
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+odd_numbers = list(filter(lambda x: x % 2 != 0, numbers))
+print(odd_numbers)
+
+
+#recursion
+def countdown(n):
+  if n <= 0:
+    print("Done!")
+  else:
+    print(n)
+    countdown(n - 1)
+
+countdown(5)
+
+
+def fibonacci(n):
+  if n <= 1:
+    return n
+  else:
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+print(fibonacci(7))
+
+#recursion depth limit
+import sys
+sys.setrecursionlimit(2000)
+print(sys.getrecursionlimit())
+
+
+#yield
+def simple_gen():
+  yield "Emil"
+  yield "Tobias"
+  yield "Linus"
+
+gen = simple_gen()
+print(next(gen))
+print(next(gen))
+print(next(gen))
