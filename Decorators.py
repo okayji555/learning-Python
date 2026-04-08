b9bf67df0cd1bc66ca78863691aecc8fa1d1ec6e
Omkar Gore <yothisisokg@gmail.com>
2026-04-08 22:57:54 +0530

    Add files via upload

---
+++ Decorators.py
@@ -0,0 +1,17 @@
+def changecase(func):
+  def myinner():
+    return func().upper()
+  return myinner
+
+def addgreeting(func):
+  def myinner():
+    return "Hello " + func() + " Have a good day!"
+  return myinner
+
+@changecase
+@addgreeting
+def myfunction():
+  return "Tobias"
+
+print(myfunction())
+
